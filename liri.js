117964842bfc64bfdcfe1df4b434fe28aa45dddc
Maeli Hector <maelihector@gmail.com>
2018-04-05 21:42:53 -0500

    Make it to where 'my-tweets' is required as arg[2] in order for tweets to console logged.

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
-// Take in the command line arguments
-var nodeArg = process.argv[2];
+// Take in the command line argument indexOf[2]
+var command = process.argv[2];
 
 // Require packages and keys file, bring into the scope of liri.js
 var dotenv = require('dotenv').config();
@@ -12,10 +12,14 @@ var keys = require('./keys');
 var client = new Twitter(keys.twitter);
 // Get statuses from 'knlsworthington' user_timeline, with 'count=20' included in the call
 client.get('statuses/user_timeline.json?screen_name=knlsworthington&count=20', function (error, data) {
-  if (error) throw error;
+  //if (error) throw error;
   // Loop through the data and console log only "text" and "created_at" data.
   for (var i = 0; i < data.length; i++) {
+    if (command === "my-tweets"){
     console.log(`Tweet: ${data[i].text}`);
     console.log(`Created at: ${data[i].created_at}`);
+    } else {
+      console.log(error);
+    }
   }
 });
