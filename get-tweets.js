bb0555577930b13c7f76c877bb45721c8eebcafb
mhector <maelihector@gmail.com>
2019-07-30 13:51:30 -0500

    Add option to get any public account's tweets.

--- get-tweets.js
+++ get-tweets.js
@@ -7,21 +7,28 @@ var fs = require('fs-extra');
 function append(output) {
   fs.appendFile("log.txt", `${output}
 `, (error) => {
-    /*console.log(error);*/ })
+    /*console.log(error);*/
+  })
 }
 
 // Function for action === 'my-tweets'
-var getTweets = function(){
+var getTweets = function () {
   // Grab twitter keys and store them in a var to use for the Twitter API call
   var client = new Twitter(keys.twitter);
+  // Create variable for the public twitter account searched
+  var publicTwitterScreenName = process.argv[3];
+  // If none entered, default to "knlsworthington"
+  if (!publicTwitterScreenName) {
+    publicTwitterScreenName = "knlsworthington";
+  }
   // Call Twitter API and 'GET' statuses of user_timeline = 'knlsworthington' up to 'count=20'. I Included paramaters in the call rather than saving them in a var...
-  client.get('statuses/user_timeline.json?screen_name=knlsworthington&count=20', function (error, data) {
+  client.get('statuses/user_timeline.json?screen_name=' + publicTwitterScreenName + '&count=20', function (error, data) {
     if (error) throw error;
     // Loop through all of the data 
     for (var i = 0; i < data.length; i++) {
       // Console log only "text" and "created_at" and call append() 
       let log = `
-      On ${data[i].created_at}, Kenilsworthington tweeted: 
+      On ${data[i].created_at}, ${publicTwitterScreenName} tweeted: 
       ${data[i].text}`
       // Call append() to append output data to log.txt
       console.log(log);
